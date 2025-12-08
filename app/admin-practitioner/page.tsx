72c15fdcadc1cc7afa278d984349dff5f4f745ac
Yumeng Cao <yumengcao4888@gmail.com>
2025-12-07 19:52:31 -0600

    Tried to fix the admin page not updating issue.

--- app/admin-practitioner/page.tsx
+++ app/admin-practitioner/page.tsx
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 
-const prisma = new PrismaClient();
+export const dynamic = "force-dynamic"
 
 export default async function PractitionerListPage() {
   const practitioners = await prisma.practitioner.findMany({
