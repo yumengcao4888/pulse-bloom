2c1b4ece7f0f267d59def978e5a0030a8ac1cfb8
Yumeng Cao <yumengcao4888@gmail.com>
2025-12-11 16:53:32 -0600

    Updated to concurrently (^9.2.1), prettier (^3.7.4), and prettier-plugin-tailwindcss (^0.7.2).

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+// prettier.config.mjs
+const config = {
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
