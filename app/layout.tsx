4f6332ed1ba0f5f11ca374685889340fcf029ba9
Yumeng Cao <yumengcao4888@gmail.com>
2025-12-21 18:51:47 -0600

    Optimized homepage display test 2.

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
             <Suspense fallback="...">
               <Navbar />
             </Suspense>
-            <main className="flex min-h-screen w-full flex-col items-center justify-center py-20">
+            <main className="flex min-h-screen w-full flex-col items-center justify-center py-10">
               {children}
             </main>
             <Footer />
