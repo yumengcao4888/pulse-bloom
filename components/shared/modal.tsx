d1f7dc826468c67d8526b2cfbaa17987e263665a
Yumeng Cao <yumengcao4888@gmail.com>
2025-12-24 00:57:31 -0600

    Fixed modal runtime error.

--- components/shared/modal.tsx
+++ components/shared/modal.tsx
@@ -32,6 +32,7 @@ export default function Modal({
               className,
             )}
           >
+            <Drawer.Title className="sr-only">{title}</Drawer.Title>
             <div className="sticky top-0 z-20 flex w-full items-center justify-center rounded-t-[10px] bg-inherit">
               <div className="my-3 h-1 w-12 rounded-full bg-gray-300" />
             </div>
