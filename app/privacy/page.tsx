432a211c40175f02f25ccd83c519e3621c2f3ada
Yumeng Cao <yumengcao4888@gmail.com>
2026-02-07 13:42:53 -0600

    Removed extra boarder in privacy page.

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -3,8 +3,7 @@ export default function PrivacyPage() {
     <div className="relative z-10 w-full max-w-2xl px-5 xl:px-0">
       <div className="mx-auto my-10 w-full">
         <div className="rounded-2xl border bg-white/70 p-6 shadow-sm">
-
-          <div className="mt-6 overflow-hidden rounded-xl border border-gray-200 bg-white">
+          <div className="mt-6 overflow-hidden rounded-xl bg-white">
             <iframe
               title="Termly Privacy Policy"
               src="/privacy-termly.html"
